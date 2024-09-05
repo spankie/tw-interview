7940d69165323f2938c53b34aa514152472c4a63
David Odohi <odohi.david@gmail.com>
2024-09-05 16:16:20 +0100

    refactor test due to lint errors

--- blockparser/blockparser_test.go
+++ blockparser/blockparser_test.go
@@ -35,115 +35,126 @@ func (m *MockBlockchainQuerier) GetBlock(_ string) (*blockchain.Block, error) {
 	return m.Block, nil
 }
 
-func TestParser(t *testing.T) {
+var sampleBlock = blockchain.Block{
+	Difficulty: "0x4ea3f27bc",
+	ExtraData:  "0x476574682f4c5649562f76312e302e302f6c696e75782f676f312e342e32",
+	GasLimit:   "0x1388",
+	GasUsed:    "0x0",
+	Hash:       "0xdc0818cf78f21a8e70579cb46a43643f78291264dda342ae31049421c82d21ae",
+	LogsBloom: "0x0000000000000000000000000000000000000000000000000" +
+		"0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000" +
+		"0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000" +
+		"0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000" +
+		"0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000" +
+		"0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000" +
+		"00000000000000000000000",
+	Miner:           "0xbb7b8287f3f0a933474a79eae42cbca977791171",
+	MixHash:         "0x4fffe9ae21f1c9e15207b1f472d5bbdd68c9595d461666602f2be20daf5e7843",
+	Nonce:           "0x689056015818adbe",
+	Number:          "0x1b4",
+	ParentHash:      "0xe99e022112df268087ea7eafaf4790497fd21dbeeb6bd7a1721df161a6657a54",
+	ReceiptsRoot:    "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
+	Sha3Uncles:      "0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347",
+	Size:            "0x220",
+	StateRoot:       "0xddc8b0234c2e0cad087c8b389aa7ef01f7d79b2570bccb77ce48648aa61c904d",
+	Timestamp:       "0x55ba467c",
+	TotalDifficulty: "0x78ed983323d",
+	Transactions: []blockchain.Transaction{
+		{
+			BlockHash:        "0x1d59ff54b1eb26b013ce3cb5fc9dab3705b415a67127a003c3e61eb445bb8df2",
+			BlockNumber:      "0x5daf3b",
+			From:             "0xa7d9ddbe1f17865597fbd27ec712455208b6b76d",
+			Gas:              "0xc350",
+			GasPrice:         "0x4a817c800",
+			Hash:             "0x88df016429689c079f3b2f6ad39fa052532c56795b733da78a91ebe6a713944b",
+			Input:            "0x68656c6c6f21",
+			Nonce:            "0x15",
+			To:               "0xf02c1c8e6114b1dbe8937a39260b5b0a374432bb",
+			TransactionIndex: "0x41",
+			Value:            "0xf3dbb76162000",
+			V:                "0x25",
+			R:                "0x1b5e176d927f8e9ab405058b2d2457392da3e20f328b16ddabcebc33eaac5fea",
+			S:                "0x4ba69724e8f69de52f0125ad8b3c5c2cef33019bac3249e2c0a2192766d1721c",
+		},
+		{
+			BlockHash:        "0x2d59ff54b1eb26b013ce3cb5fc9dab3705b415a67127a003c3e61eb445bb8df2",
+			BlockNumber:      "0x5daf3c",
+			From:             "0xa7d9ddbe1f17865597fbd27ec712455208b6b76d",
+			Gas:              "0xc351",
+			GasPrice:         "0x4a817c801",
+			Hash:             "0x88df016429689c079f3b2f6ad39fa052532c56795b733da78a91ebe6a713944c",
+			Input:            "0x68656c6c6f22",
+			Nonce:            "0x16",
+			To:               "0xf02c1c8e6114b1dbe8937a39260b5b0a374432bc",
+			TransactionIndex: "0x42",
+			Value:            "0xf3dbb76162001",
+			V:                "0x26",
+			R:                "0x1b5e176d927f8e9ab405058b2d2457392da3e20f328b16ddabcebc33eaac5feb",
+			S:                "0x4ba69724e8f69de52f0125ad8b3c5c2cef33019bac3249e2c0a2192766d1721d",
+		},
+		{
+			BlockHash:        "0x3d59ff54b1eb26b013ce3cb5fc9dab3705b415a67127a003c3e61eb445bb8df2",
+			BlockNumber:      "0x5daf3d",
+			From:             "0xa7d9ddbe1f17865597fbd27ec712455208b6b76f",
+			Gas:              "0xc352",
+			GasPrice:         "0x4a817c802",
+			Hash:             "0x88df016429689c079f3b2f6ad39fa052532c56795b733da78a91ebe6a713944d",
+			Input:            "0x68656c6c6f23",
+			Nonce:            "0x17",
+			To:               "0xf02c1c8e6114b1dbe8937a39260b5b0a374432bd",
+			TransactionIndex: "0x43",
+			Value:            "0xf3dbb76162002",
+			V:                "0x27",
+			R:                "0x1b5e176d927f8e9ab405058b2d2457392da3e20f328b16ddabcebc33eaac5fec",
+			S:                "0x4ba69724e8f69de52f0125ad8b3c5c2cef33019bac3249e2c0a2192766d1721e",
+		},
+	},
+	TransactionsRoot: "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
+	Uncles:           []string{},
+}
+
+func TestParserSubscription(t *testing.T) {
 	datastore := newMemoryDataStore[blockchain.Transaction]()
+	block := sampleBlock
 	blockchainQuerier := &MockBlockchainQuerier{
 		LatestBlock: 0x7b,
-		Block: &blockchain.Block{
-			Difficulty: "0x4ea3f27bc",
-			ExtraData:  "0x476574682f4c5649562f76312e302e302f6c696e75782f676f312e342e32",
-			GasLimit:   "0x1388",
-			GasUsed:    "0x0",
-			Hash:       "0xdc0818cf78f21a8e70579cb46a43643f78291264dda342ae31049421c82d21ae",
-			LogsBloom: "0x0000000000000000000000000000000000000000000000000" +
-				"0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000" +
-				"0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000" +
-				"0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000" +
-				"0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000" +
-				"000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000" +
-				"000000000000000000000",
-			Miner:           "0xbb7b8287f3f0a933474a79eae42cbca977791171",
-			MixHash:         "0x4fffe9ae21f1c9e15207b1f472d5bbdd68c9595d461666602f2be20daf5e7843",
-			Nonce:           "0x689056015818adbe",
-			Number:          "0x1b4",
-			ParentHash:      "0xe99e022112df268087ea7eafaf4790497fd21dbeeb6bd7a1721df161a6657a54",
-			ReceiptsRoot:    "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
-			Sha3Uncles:      "0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347",
-			Size:            "0x220",
-			StateRoot:       "0xddc8b0234c2e0cad087c8b389aa7ef01f7d79b2570bccb77ce48648aa61c904d",
-			Timestamp:       "0x55ba467c",
-			TotalDifficulty: "0x78ed983323d",
-			Transactions: []blockchain.Transaction{
-				{
-					BlockHash:        "0x1d59ff54b1eb26b013ce3cb5fc9dab3705b415a67127a003c3e61eb445bb8df2",
-					BlockNumber:      "0x5daf3b",
-					From:             "0xa7d9ddbe1f17865597fbd27ec712455208b6b76d",
-					Gas:              "0xc350",
-					GasPrice:         "0x4a817c800",
-					Hash:             "0x88df016429689c079f3b2f6ad39fa052532c56795b733da78a91ebe6a713944b",
-					Input:            "0x68656c6c6f21",
-					Nonce:            "0x15",
-					To:               "0xf02c1c8e6114b1dbe8937a39260b5b0a374432bb",
-					TransactionIndex: "0x41",
-					Value:            "0xf3dbb76162000",
-					V:                "0x25",
-					R:                "0x1b5e176d927f8e9ab405058b2d2457392da3e20f328b16ddabcebc33eaac5fea",
-					S:                "0x4ba69724e8f69de52f0125ad8b3c5c2cef33019bac3249e2c0a2192766d1721c",
-				},
-				{
-					BlockHash:        "0x2d59ff54b1eb26b013ce3cb5fc9dab3705b415a67127a003c3e61eb445bb8df2",
-					BlockNumber:      "0x5daf3c",
-					From:             "0xa7d9ddbe1f17865597fbd27ec712455208b6b76d",
-					Gas:              "0xc351",
-					GasPrice:         "0x4a817c801",
-					Hash:             "0x88df016429689c079f3b2f6ad39fa052532c56795b733da78a91ebe6a713944c",
-					Input:            "0x68656c6c6f22",
-					Nonce:            "0x16",
-					To:               "0xf02c1c8e6114b1dbe8937a39260b5b0a374432bc",
-					TransactionIndex: "0x42",
-					Value:            "0xf3dbb76162001",
-					V:                "0x26",
-					R:                "0x1b5e176d927f8e9ab405058b2d2457392da3e20f328b16ddabcebc33eaac5feb",
-					S:                "0x4ba69724e8f69de52f0125ad8b3c5c2cef33019bac3249e2c0a2192766d1721d",
-				},
-				{
-					BlockHash:        "0x3d59ff54b1eb26b013ce3cb5fc9dab3705b415a67127a003c3e61eb445bb8df2",
-					BlockNumber:      "0x5daf3d",
-					From:             "0xa7d9ddbe1f17865597fbd27ec712455208b6b76f",
-					Gas:              "0xc352",
-					GasPrice:         "0x4a817c802",
-					Hash:             "0x88df016429689c079f3b2f6ad39fa052532c56795b733da78a91ebe6a713944d",
-					Input:            "0x68656c6c6f23",
-					Nonce:            "0x17",
-					To:               "0xf02c1c8e6114b1dbe8937a39260b5b0a374432bd",
-					TransactionIndex: "0x43",
-					Value:            "0xf3dbb76162002",
-					V:                "0x27",
-					R:                "0x1b5e176d927f8e9ab405058b2d2457392da3e20f328b16ddabcebc33eaac5fec",
-					S:                "0x4ba69724e8f69de52f0125ad8b3c5c2cef33019bac3249e2c0a2192766d1721e",
-				},
-			},
-			TransactionsRoot: "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
-			Uncles:           []string{},
-		},
+		Block:       &block,
 	}
 
-	t.Run("test parser subscription", func(t *testing.T) {
-		parser := NewBlockParser(WithDataStore(datastore),
-			WithBlockchainQuerier(blockchainQuerier), WithScanningInterval(1*time.Second))
+	parser := NewBlockParser(WithDataStore(datastore),
+		WithBlockchainQuerier(blockchainQuerier), WithScanningInterval(1*time.Second))
 
-		// invalid address
-		invalidAddr := "0xabc"
-		if ok := parser.Subscribe(invalidAddr); ok {
-			t.Errorf("should not subscribe an invalid address (%s). got %v, want false", invalidAddr, ok)
-		}
+	// invalid address
+	invalidAddr := "0xabc"
+	if ok := parser.Subscribe(invalidAddr); ok {
+		t.Errorf("should not subscribe an invalid address (%s). got %v, want false", invalidAddr, ok)
+	}
 
-		addr1 := "0x26bce6ecb5b10138e4bf14ac0ffcc8727fef3b2e"
-		if ok := parser.Subscribe(addr1); !ok {
-			t.Errorf("should subscribe a valid address (%s). got %v, want true", addr1, ok)
-		}
+	addr1 := "0x26bce6ecb5b10138e4bf14ac0ffcc8727fef3b2e"
+	if ok := parser.Subscribe(addr1); !ok {
+		t.Errorf("should subscribe a valid address (%s). got %v, want true", addr1, ok)
+	}
 
-		if ok := parser.Subscribe(addr1); ok {
-			t.Errorf("should not subscribe an already subscribed address. got %v, want false", ok)
-		}
+	if ok := parser.Subscribe(addr1); ok {
+		t.Errorf("should not subscribe an already subscribed address. got %v, want false", ok)
+	}
 
-		transactions := parser.GetTransactions(addr1)
-		if len(transactions) != 0 {
-			t.Errorf("should not have any transactions for address %s. got %d, want 0", addr1, len(transactions))
-		}
-	})
+	transactions := parser.GetTransactions(addr1)
+	if len(transactions) != 0 {
+		t.Errorf("should not have any transactions for address %s. got %d, want 0", addr1, len(transactions))
+	}
+}
 
+func TestParserGetTransactions(t *testing.T) {
 	t.Run("test parser get transactions", func(t *testing.T) {
+		datastore := newMemoryDataStore[blockchain.Transaction]()
+		block := sampleBlock
+
+		blockchainQuerier := &MockBlockchainQuerier{
+			LatestBlock: 0x7b,
+			Block:       &block,
+		}
+
 		parser := NewBlockParser(WithDataStore(datastore),
 			WithBlockchainQuerier(blockchainQuerier), WithScanningInterval(1*time.Second))
 
@@ -172,6 +183,7 @@ func TestParser(t *testing.T) {
 
 		// try to query the same block again and make sure the transactions are not duplicated.
 		blockchainQuerier.LatestBlock--
+
 		parser.querySubscribedAddressTransactions(ctx)
 
 		if transactions := parser.GetTransactions(address); len(transactions) != 2 {
--- blockparser/db_test.go
+++ blockparser/db_test.go
@@ -13,59 +13,59 @@ func TestNewDataStore(t *testing.T) {
 	}
 }
 
-func TestSetAndGet(t *testing.T) {
-	store := newMemoryDataStore[blockchain.Transaction]()
+var transactions = []blockchain.Transaction{
+	{
+		BlockHash:        "0xabd",
+		BlockNumber:      "0x2",
+		From:             "0xdf0",
+		Gas:              "0x3",
+		GasPrice:         "0x4",
+		Hash:             "1xghi",
+		Input:            "0x5",
+		Nonce:            "0x6",
+		To:               "1xjkl",
+		TransactionIndex: "0x7",
+		Value:            "0x8",
+		V:                "0x9",
+		R:                "0xa",
+		S:                "0xb",
+	},
+	{
+		BlockHash:        "0xbcd",
+		BlockNumber:      "0x2",
+		From:             "0xefg",
+		Gas:              "0x3",
+		GasPrice:         "0x4",
+		Hash:             "0xhij",
+		Input:            "0x5",
+		Nonce:            "0x6",
+		To:               "0xklm",
+		TransactionIndex: "0x7",
+		Value:            "0x8",
+		V:                "0x9",
+		R:                "0xa",
+		S:                "0xb",
+	},
+	{
+		BlockHash:        "0xcde",
+		BlockNumber:      "0x3",
+		From:             "0xfgh",
+		Gas:              "0x4",
+		GasPrice:         "0x5",
+		Hash:             "0ijk",
+		Input:            "0x6",
+		Nonce:            "0x7",
+		To:               "0xlmn",
+		TransactionIndex: "0x8",
+		Value:            "0x9",
+		V:                "0xa",
+		R:                "0xb",
+		S:                "0xc",
+	},
+}
 
-	transactions := []blockchain.Transaction{
-		{
-			BlockHash:        "0xabc",
-			BlockNumber:      "0x1",
-			From:             "0xdef",
-			Gas:              "0x2",
-			GasPrice:         "0x3",
-			Hash:             "0xghi",
-			Input:            "0x4",
-			Nonce:            "0x5",
-			To:               "0xjkl",
-			TransactionIndex: "0x6",
-			Value:            "0x7",
-			V:                "0x8",
-			R:                "0x9",
-			S:                "0xa",
-		},
-		{
-			BlockHash:        "0xbcd",
-			BlockNumber:      "0x2",
-			From:             "0xefg",
-			Gas:              "0x3",
-			GasPrice:         "0x4",
-			Hash:             "0xhij",
-			Input:            "0x5",
-			Nonce:            "0x6",
-			To:               "0xklm",
-			TransactionIndex: "0x7",
-			Value:            "0x8",
-			V:                "0x9",
-			R:                "0xa",
-			S:                "0xb",
-		},
-		{
-			BlockHash:        "0xcde",
-			BlockNumber:      "0x3",
-			From:             "0xfgh",
-			Gas:              "0x4",
-			GasPrice:         "0x5",
-			Hash:             "0ijk",
-			Input:            "0x6",
-			Nonce:            "0x7",
-			To:               "0xlmn",
-			TransactionIndex: "0x8",
-			Value:            "0x9",
-			V:                "0xa",
-			R:                "0xb",
-			S:                "0xc",
-		},
-	}
+func TestAdd(t *testing.T) {
+	store := newMemoryDataStore[blockchain.Transaction]()
 
 	t.Run("test add", func(t *testing.T) {
 		err := store.Add("testKey", transactions[:1])
@@ -92,9 +92,18 @@ func TestSetAndGet(t *testing.T) {
 			t.Errorf("From = %v, want %v", gotTransactions[0].From, transactions[0].From)
 		}
 	})
+}
+
+func TestAppend(t *testing.T) {
+	store := newMemoryDataStore[blockchain.Transaction]()
 
 	t.Run("test append", func(t *testing.T) {
-		err := store.Add("testKey", transactions[1:2])
+		err := store.Add("testKey", transactions[0:1])
+		if err != nil {
+			t.Errorf("expected nil err, got: %v", err)
+		}
+
+		err = store.Add("testKey", transactions[1:2])
 		if err != nil {
 			t.Errorf("expected nil err, got: %v", err)
 		}
@@ -117,6 +126,10 @@ func TestSetAndGet(t *testing.T) {
 			t.Errorf("From = %v, want %v", gotTransactions[1].From, transactions[1].From)
 		}
 	})
+}
+
+func TestAddEmptyKey(t *testing.T) {
+	store := newMemoryDataStore[blockchain.Transaction]()
 
 	t.Run("test empty key", func(t *testing.T) {
 		if err := store.Add("", transactions); err == nil {
